d5d878108de7e0144e13b2865c9a193604b8b90b
MarkusHerb <herburger-m@gmx.at>
2026-03-20 11:21:27 +0100

    20.03.2026

--- VerwaltungKST1127/Auftragsverwaltung/Form_Rueckstand.Designer.cs
+++ VerwaltungKST1127/Auftragsverwaltung/Form_Rueckstand.Designer.cs
@@ -28,12 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea11 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend11 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea12 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend12 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dGv_AuswahlBelag = new System.Windows.Forms.DataGridView();
             this.dateTimePickerRueckstandAb = new System.Windows.Forms.DateTimePicker();
             this.chartVergueten = new System.Windows.Forms.DataVisualization.Charting.Chart();
@@ -73,17 +73,17 @@
             // 
             // chartVergueten
             // 
-            chartArea11.Name = "ChartArea1";
-            this.chartVergueten.ChartAreas.Add(chartArea11);
-            legend11.Name = "Legend1";
-            this.chartVergueten.Legends.Add(legend11);
+            chartArea1.Name = "ChartArea1";
+            this.chartVergueten.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartVergueten.Legends.Add(legend1);
             this.chartVergueten.Location = new System.Drawing.Point(289, 147);
             this.chartVergueten.Name = "chartVergueten";
-            series11.ChartArea = "ChartArea1";
-            series11.Legend = "Legend1";
-            series11.Name = "Series1";
-            this.chartVergueten.Series.Add(series11);
-            this.chartVergueten.Size = new System.Drawing.Size(998, 248);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartVergueten.Series.Add(series1);
+            this.chartVergueten.Size = new System.Drawing.Size(1180, 248);
             this.chartVergueten.TabIndex = 3;
             this.chartVergueten.Text = "chart2";
             // 
@@ -129,17 +129,17 @@
             // 
             // chartPlusMinusTwoMonths
             // 
-            chartArea12.Name = "ChartArea1";
-            this.chartPlusMinusTwoMonths.ChartAreas.Add(chartArea12);
-            legend12.Name = "Legend1";
-            this.chartPlusMinusTwoMonths.Legends.Add(legend12);
+            chartArea2.Name = "ChartArea1";
+            this.chartPlusMinusTwoMonths.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartPlusMinusTwoMonths.Legends.Add(legend2);
             this.chartPlusMinusTwoMonths.Location = new System.Drawing.Point(289, 428);
             this.chartPlusMinusTwoMonths.Name = "chartPlusMinusTwoMonths";
-            series12.ChartArea = "ChartArea1";
-            series12.Legend = "Legend1";
-            series12.Name = "Series1";
-            this.chartPlusMinusTwoMonths.Series.Add(series12);
-            this.chartPlusMinusTwoMonths.Size = new System.Drawing.Size(998, 243);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartPlusMinusTwoMonths.Series.Add(series2);
+            this.chartPlusMinusTwoMonths.Size = new System.Drawing.Size(1180, 397);
             this.chartPlusMinusTwoMonths.TabIndex = 10;
             this.chartPlusMinusTwoMonths.Text = "chart2";
             // 
@@ -246,7 +246,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1299, 684);
+            this.ClientSize = new System.Drawing.Size(1481, 837);
             this.Controls.Add(this.btnResett);
             this.Controls.Add(this.btnZweiWochenVor);
             this.Controls.Add(this.btnZweiWochenZurueck);
