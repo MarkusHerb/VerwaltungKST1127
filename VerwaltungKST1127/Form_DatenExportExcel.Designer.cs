bf926845f48d4052215ea24f00c56b5e432ee720
MarkusHerb <herburger-m@gmx.at>
2026-05-08 11:43:33 +0200

    08.05.2026

--- VerwaltungKST1127/Form_DatenExportExcel.Designer.cs
+++ VerwaltungKST1127/Form_DatenExportExcel.Designer.cs
@@ -44,6 +44,10 @@
             this.listBoxWaschtragerl = new System.Windows.Forms.ListBox();
             this.listBoxChargenprotokoll = new System.Windows.Forms.ListBox();
             this.btnExportTable = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnExportMostImportant = new System.Windows.Forms.Button();
+            this.progressBarImportantTables = new System.Windows.Forms.ProgressBar();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -59,70 +63,70 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(17, 96);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 20);
+            this.label2.Size = new System.Drawing.Size(124, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Verwaltung alt";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(209, 96);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(243, 96);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(120, 20);
+            this.label3.Size = new System.Drawing.Size(134, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Verwaltung neu";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(401, 96);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(469, 96);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.Size = new System.Drawing.Size(67, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Shuttle";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(597, 96);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(695, 97);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(94, 20);
+            this.label5.Size = new System.Drawing.Size(104, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Auswertung";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(17, 308);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(65, 20);
+            this.label6.Size = new System.Drawing.Size(71, 20);
             this.label6.TabIndex = 5;
             this.label6.Text = "Messen";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(209, 308);
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(243, 308);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(103, 20);
+            this.label7.Size = new System.Drawing.Size(115, 20);
             this.label7.TabIndex = 6;
             this.label7.Text = "Waschtragerl";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(401, 308);
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(469, 308);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(130, 20);
+            this.label8.Size = new System.Drawing.Size(146, 20);
             this.label8.TabIndex = 7;
             this.label8.Text = "Chargenprotokoll";
             // 
@@ -131,31 +135,31 @@
             this.listBoxVerwaltungAlt.FormattingEnabled = true;
             this.listBoxVerwaltungAlt.Location = new System.Drawing.Point(21, 120);
             this.listBoxVerwaltungAlt.Name = "listBoxVerwaltungAlt";
-            this.listBoxVerwaltungAlt.Size = new System.Drawing.Size(186, 147);
+            this.listBoxVerwaltungAlt.Size = new System.Drawing.Size(220, 147);
             this.listBoxVerwaltungAlt.TabIndex = 8;
             // 
             // listBoxVerwaltungNeu
             // 
             this.listBoxVerwaltungNeu.FormattingEnabled = true;
-            this.listBoxVerwaltungNeu.Location = new System.Drawing.Point(213, 120);
+            this.listBoxVerwaltungNeu.Location = new System.Drawing.Point(247, 120);
             this.listBoxVerwaltungNeu.Name = "listBoxVerwaltungNeu";
-            this.listBoxVerwaltungNeu.Size = new System.Drawing.Size(186, 147);
+            this.listBoxVerwaltungNeu.Size = new System.Drawing.Size(220, 147);
             this.listBoxVerwaltungNeu.TabIndex = 9;
             // 
             // listBoxShuttle
             // 
             this.listBoxShuttle.FormattingEnabled = true;
-            this.listBoxShuttle.Location = new System.Drawing.Point(405, 119);
+            this.listBoxShuttle.Location = new System.Drawing.Point(473, 120);
             this.listBoxShuttle.Name = "listBoxShuttle";
-            this.listBoxShuttle.Size = new System.Drawing.Size(186, 147);
+            this.listBoxShuttle.Size = new System.Drawing.Size(220, 147);
             this.listBoxShuttle.TabIndex = 10;
             // 
             // listBoxAuswertung
             // 
             this.listBoxAuswertung.FormattingEnabled = true;
-            this.listBoxAuswertung.Location = new System.Drawing.Point(597, 119);
+            this.listBoxAuswertung.Location = new System.Drawing.Point(699, 120);
             this.listBoxAuswertung.Name = "listBoxAuswertung";
-            this.listBoxAuswertung.Size = new System.Drawing.Size(186, 147);
+            this.listBoxAuswertung.Size = new System.Drawing.Size(220, 147);
             this.listBoxAuswertung.TabIndex = 11;
             // 
             // listBoxMessen
@@ -163,42 +167,86 @@
             this.listBoxMessen.FormattingEnabled = true;
             this.listBoxMessen.Location = new System.Drawing.Point(21, 331);
             this.listBoxMessen.Name = "listBoxMessen";
-            this.listBoxMessen.Size = new System.Drawing.Size(186, 147);
+            this.listBoxMessen.Size = new System.Drawing.Size(220, 147);
             this.listBoxMessen.TabIndex = 12;
             // 
             // listBoxWaschtragerl
             // 
             this.listBoxWaschtragerl.FormattingEnabled = true;
-            this.listBoxWaschtragerl.Location = new System.Drawing.Point(213, 331);
+            this.listBoxWaschtragerl.Location = new System.Drawing.Point(247, 331);
             this.listBoxWaschtragerl.Name = "listBoxWaschtragerl";
-            this.listBoxWaschtragerl.Size = new System.Drawing.Size(186, 147);
+            this.listBoxWaschtragerl.Size = new System.Drawing.Size(220, 147);
             this.listBoxWaschtragerl.TabIndex = 13;
             // 
             // listBoxChargenprotokoll
             // 
             this.listBoxChargenprotokoll.FormattingEnabled = true;
-            this.listBoxChargenprotokoll.Location = new System.Drawing.Point(405, 331);
+            this.listBoxChargenprotokoll.Location = new System.Drawing.Point(473, 331);
             this.listBoxChargenprotokoll.Name = "listBoxChargenprotokoll";
-            this.listBoxChargenprotokoll.Size = new System.Drawing.Size(186, 147);
+            this.listBoxChargenprotokoll.Size = new System.Drawing.Size(220, 147);
             this.listBoxChargenprotokoll.TabIndex = 14;
             // 
             // btnExportTable
             // 
             this.btnExportTable.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.btnExportTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
-            this.btnExportTable.Location = new System.Drawing.Point(601, 371);
+            this.btnExportTable.Location = new System.Drawing.Point(699, 331);
             this.btnExportTable.Name = "btnExportTable";
-            this.btnExportTable.Size = new System.Drawing.Size(181, 61);
+            this.btnExportTable.Size = new System.Drawing.Size(220, 61);
             this.btnExportTable.TabIndex = 15;
             this.btnExportTable.Text = "Ausgewählte Tabelle ans Excel exportieren";
             this.btnExportTable.UseVisualStyleBackColor = false;
             this.btnExportTable.Click += new System.EventHandler(this.btnExportTable_Click);
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(17, 42);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(486, 17);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Tabelle aus einer der Liste auswählen -> Button klicken -> Excel wird erstellt";
+            // 
+            // btnExportMostImportant
+            // 
+            this.btnExportMostImportant.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnExportMostImportant.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.btnExportMostImportant.Location = new System.Drawing.Point(700, 398);
+            this.btnExportMostImportant.Name = "btnExportMostImportant";
+            this.btnExportMostImportant.Size = new System.Drawing.Size(220, 61);
+            this.btnExportMostImportant.TabIndex = 17;
+            this.btnExportMostImportant.Text = "Wichtigsten Tabellen exportieren";
+            this.btnExportMostImportant.UseVisualStyleBackColor = false;
+            this.btnExportMostImportant.Click += new System.EventHandler(this.btnExportMostImportant_Click);
+            // 
+            // progressBarImportantTables
+            // 
+            this.progressBarImportantTables.Location = new System.Drawing.Point(700, 465);
+            this.progressBarImportantTables.Name = "progressBarImportantTables";
+            this.progressBarImportantTables.Size = new System.Drawing.Size(215, 12);
+            this.progressBarImportantTables.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(17, 62);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(836, 17);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Wichtigste Tabellen werden unter P:\\TEDuTOZ\\Verschiedenes\\Austauschordner_Kst146\\" +
+    "Daten_TabellenAusSqlServer gespeichert";
+            // 
             // Form_DatenExportExcel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(794, 494);
+            this.ClientSize = new System.Drawing.Size(927, 494);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.progressBarImportantTables);
+            this.Controls.Add(this.btnExportMostImportant);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.btnExportTable);
             this.Controls.Add(this.listBoxChargenprotokoll);
             this.Controls.Add(this.listBoxWaschtragerl);
@@ -216,6 +264,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form_DatenExportExcel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Datenexport";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -240,5 +289,9 @@
         private System.Windows.Forms.ListBox listBoxWaschtragerl;
         private System.Windows.Forms.ListBox listBoxChargenprotokoll;
         private System.Windows.Forms.Button btnExportTable;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnExportMostImportant;
+        private System.Windows.Forms.ProgressBar progressBarImportantTables;
+        private System.Windows.Forms.Label label10;
     }
 }
